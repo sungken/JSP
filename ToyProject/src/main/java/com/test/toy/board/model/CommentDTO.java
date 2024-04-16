dd10e9b5ad834486a5cd7d8109463340a6f41e05
ji <tlap@naver.com>
2024-04-16 17:41:45 +0900

    commit

---
+++ ToyProject/src/main/java/com/test/toy/board/model/CommentDTO.java
@@ -0,0 +1,15 @@
+package com.test.toy.board.model;
+
+import lombok.Data;
+
+@Data
+public class CommentDTO {
+	
+	private String seq;
+	private String content;
+	private String id;
+	private String regdate;
+	private String bseq;
+	private String name;
+
+}
